a7afd19527c6ead0cb9f9fcfcd6c9007092240a3
priyankagodse007 <priyankagodse007@gmail.com>
2024-09-20 15:35:38 +0530

    Update HomeController.java

--- demogithub/src/demogithub/HomeController.java
+++ demogithub/src/demogithub/HomeController.java
@@ -7,7 +7,9 @@ public class HomeController {
 	public void m2(){
 	System.out.println("this is m2");
 	}
- 
+	public void m3(){
+	System.out.println("this is m3");
+		}
 	public static void main(String[] args) {
 		
 		System.out.println("github_179");
