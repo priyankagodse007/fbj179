d8765d38ca65be1d079e121e88c3f6e001c19273
priyankagodse007 <priyankagodse007@gmail.com>
2024-09-20 15:19:35 +0530

    git1

--- demogithub/src/demogithub/HomeController.java
+++ demogithub/src/demogithub/HomeController.java
@@ -4,6 +4,9 @@ public class HomeController {
 	public void m1() {
 		System.out.println("this is m1");
 	}
+	public void m2(){
+	System.out.println("this is m2");
+	}
  
 	public static void main(String[] args) {
 		
