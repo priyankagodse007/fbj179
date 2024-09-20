e62f9912487322f6df6ccfa6e04d7151fe7a6467
dell <dell@DESKTOP-E8KOQA0>
2024-09-20 15:09:54 +0530

    by priyanka

--- demogithub/src/demogithub/HomeController.java
+++ demogithub/src/demogithub/HomeController.java
@@ -1,7 +1,12 @@
 package demogithub;
 
 public class HomeController {
+	public void m1() {
+		System.out.println("this is m1");
+	}
+ 
 	public static void main(String[] args) {
+		
 		System.out.println("github_179");
 		
 	}
